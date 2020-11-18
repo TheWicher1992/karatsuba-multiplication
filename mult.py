3d8c12cf82c904dc33b104bd67cbd98adc3e7489
TheWicher1992 <sameer.nadeem24@gmail.com>
2020-11-18 19:45:03 +0500

    Final Karatsuba

---
+++ mult.py
@@ -0,0 +1,27 @@
+def karatsuba(x, y):
+    lx = len(x)
+    ly = len(y)
+    
+    if lx < 2 or ly < 2:
+        return int(x) * int(y)
+    n = lx
+
+    if(ly > lx):
+        x = x.zfill(ly)
+        n = ly
+    if(lx > ly):
+        y = y.zfill(lx)
+        n = lx
+
+    m = int(n/2)
+
+    a = x[0:n-m]
+    b = x[n-m:n]
+    c = y[0:n-m]
+    d = y[n-m:n]
+
+    e = karatsuba(a, c)
+    f = karatsuba(b, d)
+    g = karatsuba(b, c)
+    h = karatsuba(a, d)
+    return e * 10**(2 * m) + (g+h) * 10**m + f
